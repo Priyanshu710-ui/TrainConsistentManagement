6a4cb99cfa5fed9cbd7e67e40772dd18c3ef5893
Priyanshu710-ui <nanu66292@gmail.com>
2026-04-10 09:29:27 +0530

    Sort Bogies by Capacity (Comparator)

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,36 +1,48 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class TrainConsistApp {
+class Bogie {
+    private String name;
+    private int capacity;
 
 
+    public Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+
+    public void display() {
+        System.out.println(name + " Bogie - Capacity: " + capacity);
+    }
+}
+
+public class TrainConsistentManagementApp {
     public static void main(String[] args) {
 
-        System.out.println("===============================================");
-        System.out.println("UC6 - Map Bogie to Capacity (HashMap)");
-        System.out.println("===============================================\n");
+
+        List<Bogie> bogieList = new ArrayList<>();
 
 
-        Map<String, Integer> bogieCapacityMap = new HashMap<>();
+        bogieList.add(new Bogie("Sleeper", 72));
+        bogieList.add(new Bogie("AC Chair", 56));
+        bogieList.add(new Bogie("First Class", 24));
 
 
-        bogieCapacityMap.put("Sleeper", 72);
-        bogieCapacityMap.put("AC Chair", 60);
-        bogieCapacityMap.put("First Class", 24);
-        bogieCapacityMap.put("Cargo", 100);
+        bogieList.sort(Comparator.comparingInt(Bogie::getCapacity));
 
 
-        System.out.println("Bogie Capacity Details:\n");
-
-        for (Map.Entry<String, Integer> entry : bogieCapacityMap.entrySet()) {
-            System.out.println("Bogie: " + entry.getKey() +
-                    " | Capacity: " + entry.getValue());
+        System.out.println("Bogies Sorted by Capacity (Ascending):");
+        for (Bogie b : bogieList) {
+            b.display();
         }
-
-        System.out.println("\nNote:");
-        System.out.println("HashMap stores bogies as keys and capacities as values.");
-        System.out.println("Allows fast lookup and efficient data management.");
-
-        System.out.println("\nUC6 mapping setup completed...");
     }
 }
